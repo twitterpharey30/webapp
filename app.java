7b65e54d07fe6bcaa8838efb91bed59e65435039
Balogun Fareed <129180120+BalogunFareed@users.noreply.github.com>
2025-10-22 15:56:01 +0100

    Update app.java

--- app.java
+++ app.java
@@ -1,4 +1,5 @@
-# This is cod for a web application
-# Please do not hard code
+# This is code for a web application
+# Please do not hard-code
 # make use of variables and dynamic variables
 # author: Balogun Fareed
+# This is a fe application
